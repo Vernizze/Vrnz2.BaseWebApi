04de9768e0b8d3969d631dd835e064b6d0d0931c
Vernizze <cfvernizze@gmail.com>
2020-12-16 14:00:48 -0300

    Inclusao do Validador da request 'Ping'

---
+++ Vrnz2.BaseWebApi/Validations/PingRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Vrnz2.BaseWebApi.Validations
+{
+    public class PingRequestValidator
+        : AbstractValidator<BaseContracts.DTOs.Ping.Request>
+    {
+        #region Constructors
+
+        public PingRequestValidator() { }
+
+        #endregion
+    }
+}
